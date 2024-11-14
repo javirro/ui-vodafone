d48f8787fe9f4c89af09f5fa291789342de233c1
javirrof <javirofe96@gmail.com>
2024-11-14 15:51:19 +0100

    Add navbar design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,24 @@
+import { NavLink } from 'react-router-dom'
 import vodafoneLogo from '../../images/vodafone-logo.webp'
+
 import './Navbar.css'
 const Navbar = () => {
-  return <nav>
-    <div className='content'>
-      <div className='upper'>
-
+  return (
+    <nav>
+      <div className="content">
+        <div className="upper">
+          <span>Enterprise</span>
+        </div>
+        <div className="lower">
+          <div className="left">
+            <img src={vodafoneLogo} alt="Vodafone logo" />
+            <NavLink to="/" className="link">Home</NavLink>
+            <NavLink to="/devices" className="link">Devices</NavLink>
+          </div>
+        </div>
       </div>
-      <div className='lower'>
-        <img src={vodafoneLogo} alt='Vodafone logo' />
-      </div>
-    </div>
-  </nav>
+    </nav>
+  )
 }
 
 export default Navbar
